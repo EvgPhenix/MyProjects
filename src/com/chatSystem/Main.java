3c41b5b14322751e24552ddaa326accf1c660793
Evgeniy Losev <phenix.e@yandex.ru>
2018-11-09 09:36:39 -0500

    started 09.11.2018

---
+++ src/com/chatSystem/Main.java
@@ -0,0 +1,8 @@
+package com.chatSystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/chatSystem/MessageType.java
@@ -0,0 +1,7 @@
+package com.chatSystem;
+
+public enum MessageType {
+
+    NAME_REQUEST, USER_NAME, NAME_ACCEPTED, TEXT, USER_ADDED, USER_REMOVED
+
+}
